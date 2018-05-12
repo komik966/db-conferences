3e60ad356d7d577da21aa338b03bbbab1b100995
konradmikucki <komik966@gmail.com>
2018-05-12 06:40:16 +0200

    cleaning

--- sql/04_orders.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE orders (
-  id               INT                IDENTITY CONSTRAINT pk_orders PRIMARY KEY,
-  customer_id      INT       NOT NULL CONSTRAINT fk_orders_customer FOREIGN KEY REFERENCES customers,
-  order_date       DATETIME2 NOT NULL CONSTRAINT default_orders_order_date DEFAULT CURRENT_TIMESTAMP,
-  payment_due_date DATETIME2 NOT NULL CONSTRAINT default_orders_payment_due_date DEFAULT DATEADD(WEEK, 1, CURRENT_TIMESTAMP)
-);
-
-INSERT INTO orders (customer_id) VALUES (1);
-INSERT INTO orders (customer_id) VALUES (2);
--- sql/05_payments.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE payments (
-  order_id     INT CONSTRAINT fk_payments_order FOREIGN KEY REFERENCES orders CONSTRAINT pk_payments PRIMARY KEY,
-  payment_date DATETIME2 NOT NULL CONSTRAINT default_payments_payment_date DEFAULT CURRENT_TIMESTAMP,
-);
-
-INSERT INTO payments (order_id) VALUES (1);
-INSERT INTO payments (order_id) VALUES (2);
