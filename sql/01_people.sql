02528f72f80ffc8fd4935cfc80b97fed81dd1979
konradmikucki <komik966@gmail.com>
2018-05-20 13:24:44 +0200

    student card, removed inserts

--- sql/01_people.sql
+++ sql/01_people.sql
@@ -4,5 +4,8 @@ CREATE TABLE people (
   second_name VARCHAR(255) NOT NULL,
 );
 
-INSERT INTO people (first_name, second_name) VALUES ('Lyla', 'Heaney');
-INSERT INTO people (first_name, second_name) VALUES ('Willis', 'McClure');
+CREATE TABLE student_card (
+  id     INT IDENTITY CONSTRAINT pk_student_card PRIMARY KEY,
+  number VARCHAR(32) NOT NULL,
+  person_id NOT NULL CONSTRAINT unique_student_card_person UNIQUE CONSTRAINT fk_student_card_person FOREIGN KEY REFERENCES people
+);
--- sql/02_customers.sql
+++ sql/02_customers.sql
@@ -5,17 +5,10 @@ CREATE TABLE customers (
 
 CREATE TABLE customer_individual (
   customer_id INT CONSTRAINT pk_customer_individual PRIMARY KEY CONSTRAINT fk_customer_individual_customer FOREIGN KEY REFERENCES customers,
-  person_id   INT NOT NULL CONSTRAINT unique_customer_individual_person UNIQUE CONSTRAINT fk_customer_individual_person FOREIGN KEY REFERENCES people,
-  student_card_number VARCHAR(32) DEFAULT NULL -- przenieść do osobnej tabeli
+  person_id   INT NOT NULL CONSTRAINT unique_customer_individual_person UNIQUE CONSTRAINT fk_customer_individual_person FOREIGN KEY REFERENCES people
 );
 
 CREATE TABLE companies (
   customer_id INT          NOT NULL CONSTRAINT fk_companies_customer FOREIGN KEY REFERENCES customers,
   name        VARCHAR(255) NOT NULL
 );
-
-INSERT INTO customers (phone_number) VALUES ('0048 056 120 630');
-INSERT INTO customers (phone_number) VALUES ('0048 147 948 157');
-INSERT INTO customer_individual (customer_id, person_id) VALUES (1, 1);
-
-INSERT INTO companies (customer_id, name) VALUES (2, 'Harris Inc');
