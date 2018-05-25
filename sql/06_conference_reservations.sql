3230771bbc7afc525dc238b472bde9af55081e6f
konradmikucki <komik966@gmail.com>
2018-05-25 19:09:56 +0200

    conference attendees amount check

--- sql/06_conference_reservations.sql
+++ sql/06_conference_reservations.sql
@@ -12,12 +12,12 @@ CREATE TABLE conference_reservation_details (
   id                        INT IDENTITY,
   conference_day_id         INT NOT NULL,
   conference_reservation_id INT NOT NULL,
-  -- TODO: suma attendees_amount nie może być większa od conferences.maximum_attendee_capacity
   attendees_amount          INT NOT NULL,
 
   CONSTRAINT pk_conference_reservation_details PRIMARY KEY (id),
   CONSTRAINT fk_conference_reservation_details_conference_day FOREIGN KEY (conference_day_id) REFERENCES conference_days,
   CONSTRAINT fk_conference_reservation_details_conference_reservation FOREIGN KEY (conference_reservation_id) REFERENCES conference_reservations,
+  CONSTRAINT uq_conference_reservation_details_conference_day_reservation UNIQUE (conference_day_id, conference_reservation_id),
 );
 
 CREATE TABLE conference_attendees (
@@ -29,3 +29,22 @@ CREATE TABLE conference_attendees (
   CONSTRAINT fk_conference_attendees_person FOREIGN KEY (person_id) REFERENCES people,
   CONSTRAINT fk_conference_attendees_conference_reservation_detail FOREIGN KEY (conference_reservation_detail_id) REFERENCES conference_reservation_details,
 );
+
+CREATE TRIGGER conference_reservation_details_attendees_amount
+  ON conference_reservation_details
+  AFTER INSERT, UPDATE AS
+  IF ((SELECT c.maximum_attendee_capacity
+       FROM inserted
+         INNER JOIN conference_days cd
+           ON inserted.conference_day_id = cd.id
+         INNER JOIN conferences c ON cd.conference_id = c.id) < (SELECT SUM(crd.attendees_amount)
+                                                                 FROM inserted
+                                                                   INNER JOIN conference_reservation_details crd
+                                                                     ON crd.conference_day_id =
+                                                                        inserted.conference_day_id
+                                                                 GROUP BY crd.conference_day_id))
+    BEGIN
+      RAISERROR ('Attendees amount for this conference was exceeded.', 16, 1);
+      ROLLBACK TRANSACTION;
+      RETURN
+    END;
