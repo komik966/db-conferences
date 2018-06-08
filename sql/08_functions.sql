4996e8f7ac22674404a4454b72e0423863be5b49
konradmikucki <komik966@gmail.com>
2018-06-08 03:56:39 +0200

    date range function

---
+++ sql/08_functions.sql
@@ -0,0 +1,12 @@
+CREATE FUNCTION dbo.date_range(@start_date DATE, @end_date DATE)
+  RETURNS TABLE AS RETURN
+  (
+  WITH date_range
+  AS (SELECT @start_date AS date
+      UNION ALL
+      SELECT DATEADD(DAY, 1, date)
+      FROM date_range
+      WHERE DATEADD(DAY, 1, date) <= @end_date)
+  SELECT date
+  FROM date_range
+  );
