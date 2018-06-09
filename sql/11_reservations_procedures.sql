298c3584bbac44e0380c99ee5f59168b7235ef83
konradmikucki <komik966@gmail.com>
2018-06-09 06:20:10 +0200

    create reservation procedure

--- sql/11_reservations_procedures.sql
+++ sql/11_reservations_procedures.sql
@@ -1,3 +1,22 @@
--- CREATE PROCEDURE create_reservation
---   @customer_id INT,
+CREATE PROCEDURE create_reservation
+    @customer_id       INT,
+    @conference_day_id INT,
+    @attendees_amount  INT
+AS
+  BEGIN TRANSACTION
 
+  BEGIN TRY
+
+  INSERT INTO conference_reservations (customer_id) VALUES (@customer_id);
+  DECLARE @conference_reservation_id INT;
+  SET @conference_reservation_id = SCOPE_IDENTITY();
+  INSERT INTO conference_reservation_details VALUES (@conference_day_id, @conference_reservation_id, @attendees_amount)
+
+  COMMIT;
+  END TRY
+
+  BEGIN CATCH
+  ROLLBACK;
+  THROW;
+  END CATCH;
+GO;
