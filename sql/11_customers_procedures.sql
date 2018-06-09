43269391eadc9ecaaa14d7a1b9ab8f63b28b6ecd
konradmikucki <komik966@gmail.com>
2018-06-09 03:45:53 +0200

    create customers procedures

---
+++ sql/11_customers_procedures.sql
@@ -0,0 +1,47 @@
+CREATE PROCEDURE create_company_customer
+    @phone_number VARCHAR(32),
+    @company_name VARCHAR(255),
+    @nip          VARCHAR(32)
+AS
+  BEGIN TRANSACTION
+
+  BEGIN TRY
+
+  INSERT INTO customers VALUES (@phone_number);
+  DECLARE @customer_id INT;
+  SET @customer_id = SCOPE_IDENTITY();
+  INSERT INTO companies VALUES (@customer_id, @company_name, @nip);
+
+  COMMIT;
+  END TRY
+
+  BEGIN CATCH
+  ROLLBACK;
+  THROW;
+  END CATCH;
+GO ;
+
+CREATE PROCEDURE create_individual_customer
+    @phone_number VARCHAR(32),
+    @first_name   VARCHAR(255),
+    @second_name  VARCHAR(255)
+AS
+  BEGIN TRANSACTION
+
+  BEGIN TRY
+
+  INSERT INTO customers VALUES (@phone_number);
+  DECLARE @customer_id INT, @person_id INT;
+  SET @customer_id = SCOPE_IDENTITY();
+  INSERT INTO people VALUES (@first_name, @second_name);
+  SET @person_id = SCOPE_IDENTITY();
+  INSERT INTO customer_individual VALUES (@customer_id, @person_id);
+
+  COMMIT;
+  END TRY
+
+  BEGIN CATCH
+  ROLLBACK;
+  THROW;
+  END CATCH;
+GO;
--- sql/procedures_prototype.sql
+++ sql/procedures_prototype.sql
@@ -1,13 +1,4 @@
--- CREATE PROCEDURE create_company_customer
---     @phone_number VARCHAR(32),
---     @company_name         VARCHAR(255),
---     @nip          VARCHAR(32)
---   -- insert customer, then insert company
 
--- CREATE PROCEDURE create_individual_customer
---     @phone_number VARCHAR(32),
---     @first_name   VARCHAR(255),
---     @second_name  VARCHAR(255)
 
 -- CREATE PROCEDURE pay_for_reservation
   -- insert current date to payment_date
